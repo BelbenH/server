32e02f7eaa284e50c4b775d7dec980c0ea25d344
Dallan <62350957+dallano@users.noreply.github.com>
2025-12-06 22:38:03 -0700

    old error
    Fixed forgotten error

--- scripts/effects/leavegame.lua
+++ scripts/effects/leavegame.lua
@@ -42,7 +42,7 @@ effectObject.onEffectTick = function(target, effect)
     -- but it does change the message displayed.
     local elapsedTicks = effect:getTickCount()
     if elapsedTicks > 5 then
-        xi.xispal.resetFollowers(target) -- XISP Change
+        xi.xispchocobo.despawnChocobo(player) -- XISP Change
         target:leaveGame()
     else
         target:messageSystem(messages[effect:getPower()], 30 - elapsedTicks * 5)
