ba65b86fc23e95436b2fb0d862fc346ab8614c38
Hawks <121060947+BelbenH@users.noreply.github.com>
2025-12-17 13:39:06 -0600

    xispalphamob and droplist change

--- scripts/globals/xispalphamob.lua
+++ scripts/globals/xispalphamob.lua
@@ -10,13 +10,23 @@ local familyData =
     [xi.mobSuperFamily.RABBIT] =
     {
         skillID = 404,
-        name    = { "Alpha Jack", "Grim Hare", "Big Thumper", "Mad Hopper", "Bunzilla", },
+        name    = { "Alpha Jack", "Grim Hare", "Big Thumper", "Mad Hopper", "Bunzilla", }, --mob_mod.lua 
         [tierOne]   = { dropList = 4500, groupID = 200, look = 268,  mobMods = { [xi.mobMod.EXP_BONUS] = 150, [xi.mobMod.GIL_MIN] = 500,  [xi.mobMod.GIL_MAX] = 1000, }, mods = { [xi.mod.EVA] = 10, }, },
         [tierTwo]   = { dropList = 4501, groupID = 201, look = 2955, mobMods = { [xi.mobMod.EXP_BONUS] = 200, [xi.mobMod.GIL_MIN] = 1000, [xi.mobMod.GIL_MAX] = 2500, }, mods = { [xi.mod.EVA] = 15, }, },
         [tierThree] = { dropList = 4502, groupID = 202, look = 1937, mobMods = { [xi.mobMod.EXP_BONUS] = 250, [xi.mobMod.GIL_MIN] = 2500, [xi.mobMod.GIL_MAX] = 5000, }, mods = { [xi.mod.EVA] = 25, }, },
     },
+
+    [xi.mobSuperFamily.MANDRAGORA] =
+    {
+        skillID = 496,
+        name    = { "Mandragore", "Root Reaper", "Sproutbane", "Sapling Hex", "Briar Imp", },
+        [tierOne]   = { dropList = 4503, groupID = 200, look = 268,  mobMods = { [xi.mobMod.EXP_BONUS] = 150, [xi.mobMod.GIL_MIN] = 500,  [xi.mobMod.GIL_MAX] = 1000, }, mods = { [xi.mod.REGAIN] = 100, }, },
+        [tierTwo]   = { dropList = 4504, groupID = 201, look = 2955, mobMods = { [xi.mobMod.EXP_BONUS] = 200, [xi.mobMod.GIL_MIN] = 1000, [xi.mobMod.GIL_MAX] = 2500, }, mods = { [xi.mod.REGAIN] = 100, }, },
+        [tierThree] = { dropList = 4505, groupID = 202, look = 1937, mobMods = { [xi.mobMod.EXP_BONUS] = 250, [xi.mobMod.GIL_MIN] = 2500, [xi.mobMod.GIL_MAX] = 5000, }, mods = { [xi.mod.REGAIN] = 100, }, },
+    },
 }
 
+-- DON'T TOUCH ANYTHING BELOW THIS LINE -- THIS IS THE TEMPLATE FOR HOW ALL OTHER MOBS ARE SETUP BY DEFAULT! --
 xi.alphamob.spawnAlpha = function(mob, player, phList)
     local roll     = math.random(1, 100)
     local mobLvl   = mob:getMainLvl()
@@ -48,7 +58,7 @@ xi.alphamob.spawnAlpha = function(mob, player, phList)
         local alphaLvl     = mobLvl
 
         if not alphaData then
-            print("DEBUG: No alpha data for mob super family ID " .. mob:getMobSuperFamily())
+            print("DEBUG: No alpha data for mob super family ID " .. mob:getSuperFamily())
             return
         end
 
