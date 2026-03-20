120c93b42fe9a8100ad672c06f2685247cda9f0d
Hawks <121060947+BelbenH@users.noreply.github.com>
2026-03-20 18:12:35 -0500

    Promy ez-mode enabled

--- modules/cop/sql/cop_level_restrictions.sql
+++ modules/cop/sql/cop_level_restrictions.sql
@@ -5,11 +5,11 @@
 -- Source: https://www.bg-wiki.com/ffxi/Version_Update_(06/21/2010)
 -----------------------------------
 
-UPDATE `zone_settings` SET restriction = 30 WHERE `name` = "Promyvion-Holla";
-UPDATE `zone_settings` SET restriction = 30 WHERE `name` = "Promyvion-Dem";
-UPDATE `zone_settings` SET restriction = 30 WHERE `name` = "Promyvion-Mea";
-UPDATE `zone_settings` SET restriction = 50 WHERE `name` = "Promyvion-Vahzl";
-UPDATE `zone_settings` SET restriction = 40 WHERE `name` = "Phomiuna_Aqueducts";
-UPDATE `zone_settings` SET restriction = 50 WHERE `name` = "Sacrarium";
-UPDATE `zone_settings` SET restriction = 50 WHERE `name` = "Riverne-Site_B01";
-UPDATE `zone_settings` SET restriction = 40 WHERE `name` = "Riverne-Site_A01";
+UPDATE `zone_settings` SET restriction = 35 WHERE `name` = "Promyvion-Holla";
+UPDATE `zone_settings` SET restriction = 35 WHERE `name` = "Promyvion-Dem";
+UPDATE `zone_settings` SET restriction = 35 WHERE `name` = "Promyvion-Mea";
+UPDATE `zone_settings` SET restriction = 55 WHERE `name` = "Promyvion-Vahzl";
+UPDATE `zone_settings` SET restriction = 45 WHERE `name` = "Phomiuna_Aqueducts";
+UPDATE `zone_settings` SET restriction = 55 WHERE `name` = "Sacrarium";
+UPDATE `zone_settings` SET restriction = 55 WHERE `name` = "Riverne-Site_B01";
+UPDATE `zone_settings` SET restriction = 45 WHERE `name` = "Riverne-Site_A01";
